18acd9109c7a9289c62af5cdc9228c4e4580e668
el.beress@gmail.com <el.beressa@gmail.com>
2021-01-28 17:03:01 +0100

    Jeudi

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ import './styles/global.css'
 
 
 export class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      water: 0,
+      heart: 120,
+      tempertature: -10,
+      steps: 3000,
+
+
+
+    }
+  }
 
   tempMin = -20;
   tempMax = 40;
@@ -13,17 +25,37 @@ export class App extends React.Component {
   heartMax = 180;
   stepsMin = 0;
   stepsMax = 50000;
+  onHeartChange = (event) => {
+    this.setState({ heart: event.target.value });
 
+  }
+  onTempsChange = (event) => {
+    this.setState({ tempertature: event.target.value });
+  }
+  onStepChange = (event) => {
+    this.setState({ steps: event.target.value });
+  }
+  calculateWater = () => {
+    let water = 1.5 {
+      if (condition) {
+        
+      }
+    }
+  }
   render() {
+
+
 
     return (
 
       <div className='container-fluid'>
         <div className='row'>
-          <Box icon={"directions_walk" } color={"red"}/>
-          <p>Heart : {this.heartMin}</p>
-          <p>Temperature : {this.tempMin}</p>
-          <p> Steps : {this.stepsMin}</p>
+
+
+          <Box toto="water" icon={"local_drink"} color={"#3A85FF"} value={1.5} unit="L" />
+          <Box toto="walk" icon={"directions_walk"} color={"black"} value={this.state.steps} min={this.stepsMin} max={this.stepsMax} onChange={this.onStepChange} unit="steps" />
+          <Box toto="heart" icon={"favorite"} color={"red"} min={this.heartMin} max={this.heartMax} value={this.state.heart} onChange={this.onHeartChange} unit="bpm" />
+          <Box toto="temp" icon={"wb_sunny"} color={"yellow"} min={this.tempMin} max={this.tempMax} value={this.state.tempertature} onChange={this.onTempsChange} unit="°C" />
         </div>
       </div>
     );
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -2,14 +2,41 @@ import React from "react";
 
 class Box extends React.Component {
     render() {
-        return (
-            <div className="box col-sm-3 col-6">
-                <span className="material-icons" style={{color:this.props.color, fontSize:"100px"}}>   
+        if (this.props.toto === "water") {
+            return (<div className="box col-sm-3 col-6">
+                <span className="material-icons" style={{ color: this.props.color, fontSize: "100px" }}>
                     {this.props.icon}
-                    </span>
-                <p> Welcome to my amazing blog</p>
-            </div>
-        )
+
+                </span>
+
+                <p>{this.props.value}</p>
+                <p>{this.props.unit}</p>
+            </div>)
+
+        } else {
+            return (<div className="box col-sm-3 col-6">
+                <span className="material-icons" style={{ color: this.props.color, fontSize: "100px" }}>
+                    {this.props.icon}
+
+                </span>
+                <div>
+                    <input type="range"
+                        min={this.props.min}
+                        max={this.props.max}
+                        value={this.props.value}
+                        onInput={this.props.onChange}>
+                        
+                        </input>
+                </div>
+
+
+
+                <p>{this.props.value}</p>
+                <p>{this.props.unit}</p>
+            </div>)
+
+        }
+
     };
 }
 
